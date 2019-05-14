5e0c913f4ea6e51f0dfe24100574431e448dba55
popiette <pier.jacquot@gmail.com>
2019-05-14 22:23:41 +0200

    Not using hardcoded length of tabs anymore.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #include "Effect.h"
 #include "ResetEffect.h"
 
+const int colorNumber = 17;
 const char * const colors[] = { "default", "black", "red", "green", 
 	"yellow", "blue", "magenta", "cyan", "lgray", "dgray", "lred", 
 	"lgreen", "lyellow", "lblue", "lmagenta", "lcyan", "white"
@@ -17,6 +18,7 @@ const ColorIndex colorIndexTab[] = { DEFAULT, BLACK, RED, GREEN,
 		DARK_GRAY, LIGHT_RED, LIGHT_GREEN, LIGHT_YELLOW, 
 		LIGHT_BLUE, LIGHT_MAGENTA, LIGHT_CYAN, WHITE};
 
+const int effectNumber = 8;
 const char * const effects[] = { "bold", "dim", "italic", "underlined",
 	"blink", "inverted", "hidden", "all"
 	};
@@ -29,35 +31,33 @@ const EffectIndex effectIndexTab[] = {BOLD, DIM, ITALIC,
 void setColor(char * strColor)
 {
 	int i = 0;
-	//TODO: do not use 17 hardcoded.
-	while(i < 17 && strcmp(strColor, colors[i]) != 0)
+	while(i < colorNumber && strcmp(strColor, colors[i]) != 0)
 	{ ++i; }
-	if(i < 17)
+	if(i < colorNumber)
 		cout << Color(colorIndexTab[i]);	
 }
 
 void setBackground(char * strColor)
 {
 	int i = 0;
-	//TODO: do not use 17 hardcoded.
-	while(i < 17 && strcmp(strColor, colors[i]) != 0)
+	while(i < colorNumber && strcmp(strColor, colors[i]) != 0)
 	{ ++i; }
-	if(i < 17)
+	if(i < colorNumber)
 		cout << Background(colorIndexTab[i]);
 }
 
 void setEffect(char * strEffect)
 {
 	int i = 0;
-	while(i < 8 && strcmp(strEffect, effects[i]) != 0)
+	while(i < effectNumber && strcmp(strEffect, effects[i]) != 0)
 	{ ++i; }
-	if(i < 8)
+	if(i < effectNumber)
 		cout << Effect(effectIndexTab[i]);
 }
 
 int main(int argc, char ** argv)
 {
-	for(unsigned int i = 1; i < argc-1; ++i)
+	for(int i = 1; i < argc-1; ++i)
 	{
 		if(strcmp(argv[i], "-c") == 0)
 		{
