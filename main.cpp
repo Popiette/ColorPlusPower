c14c22b3506f958230a1282dcf3d22ca294694fb
popiette <pier.jacquot@gmail.com>
2019-07-28 11:21:36 +0200

    now using TCLAP to refactor code.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <string>
+#include <tclap/CmdLine.h>
 using namespace std;
 
 #include "Color.h"
@@ -8,7 +10,7 @@ using namespace std;
 #include "ResetEffect.h"
 
 const int colorNumber = 17;
-const char * const colors[] = { "default", "black", "red", "green", 
+const string colors[] = { "default", "black", "red", "green", 
 	"yellow", "blue", "magenta", "cyan", "lgray", "dgray", "lred", 
 	"lgreen", "lyellow", "lblue", "lmagenta", "lcyan", "white"
 	};
@@ -19,7 +21,7 @@ const ColorIndex colorIndexTab[] = { DEFAULT, BLACK, RED, GREEN,
 		LIGHT_BLUE, LIGHT_MAGENTA, LIGHT_CYAN, WHITE};
 
 const int effectNumber = 8;
-const char * const effects[] = { "bold", "dim", "italic", "underlined",
+const string effects[] = { "bold", "dim", "italic", "underlined",
 	"blink", "inverted", "hidden", "all"
 	};
 
@@ -28,37 +30,37 @@ const EffectIndex effectIndexTab[] = {BOLD, DIM, ITALIC,
 
 
 
-void setColor(char * strColor)
+void setColor(const string &strColor)
 {
 	int i = 0;
-	while(i < colorNumber && strcmp(strColor, colors[i]) != 0)
+	while(i < colorNumber && strColor != colors[i])
 	{ ++i; }
 	if(i < colorNumber)
 		cout << Color(colorIndexTab[i]);	
 }
 
-void setBackground(char * strColor)
+void setBackground(const string &strColor)
 {
 	int i = 0;
-	while(i < colorNumber && strcmp(strColor, colors[i]) != 0)
+	while(i < colorNumber && strColor != colors[i])
 	{ ++i; }
 	if(i < colorNumber)
 		cout << Background(colorIndexTab[i]);
 }
 
-void setEffect(char * strEffect)
+void setEffect(const string &strEffect)
 {
 	int i = 0;
-	while(i < effectNumber && strcmp(strEffect, effects[i]) != 0)
+	while(i < effectNumber && strEffect != effects[i])
 	{ ++i; }
 	if(i < effectNumber)
 		cout << Effect(effectIndexTab[i]);
 }
 
-void resetEffect(char * strReset)
+void resetEffect(const string &strReset)
 {
 	int i = 0;
-	while(i < effectNumber && strcmp(strReset, effects[i]) != 0)
+	while(i < effectNumber && strReset != effects[i])
 	{ ++i; }
 	if(i < effectNumber)
 		cout << ResetEffect(effectIndexTab[i]);
@@ -72,40 +74,78 @@ void reset()
 
 int main(int argc, char ** argv)
 {
+	TCLAP::CmdLine command("ColorPlusPower !", ' ', "0.9");
+	TCLAP::ValueArg<string> colorArg("c", 
+				"color", 
+				"color to set", 
+				false, 
+				"default", 
+				"string");
+	command.add(colorArg);
+
+	TCLAP::ValueArg<string> backgroundArg("b",
+				"background",
+				"background color to set",
+				false,
+				"default",
+				"string");
+	command.add(backgroundArg);
+
+	TCLAP::ValueArg<string> effectArg("e",
+				"effect",
+				"effect to put on text",
+				false,
+				"default",
+				"string");
+	command.add(effectArg);
+
+	TCLAP::ValueArg<string> resetEffectArg("r",
+				"reset",
+				"remove effect from text",
+				false,
+				"default",
+				"string");
+	command.add(resetEffectArg);
+
+	TCLAP::SwitchArg resetTermSwitch("z",
+				"noreset",
+				"don't reset terminal properties when command finished",
+				command,
+				false);
+
+	TCLAP::SwitchArg dontEndlSwitch("n",
+				"noendl",
+				"don't no line return when command finish",
+				command,
+				false);
+
+	TCLAP::UnlabeledValueArg<string> textArg ("text", 
+						"text to print",
+						false,
+						"default text",
+						"nameString");
+
+	command.add(textArg);
+
+	command.parse(argc, argv);	
 	//Do we need to reset parameters at the end of the program ?
-	bool resetTerminal = true;
-	bool doEndl = true;
-	for(int i = 1; i < argc; ++i)
-	{
-		if(strcmp(argv[i], "--noreset") == 0)
-		{
-			resetTerminal = false;
-		}
-		else if(strcmp(argv[i], "--noendl") == 0)
-		{
-			doEndl = false;
-		}
-		else if(strcmp(argv[i], "-c") == 0 && i < argc - 1)
-		{
-			setColor(argv[++i]);	
-		}
-		else if(strcmp(argv[i], "-b") == 0 && i < argc - 1)
-		{
-			setBackground(argv[++i]);
-		}
-		else if(strcmp(argv[i], "-e") == 0 && i < argc - 1)
-		{
-			setEffect(argv[++i]);
-		}
-		else if(strcmp(argv[i], "-r") == 0 && i < argc - 1)
-		{
-			resetEffect(argv[++i]);
-		}
-		else
-		{
-			cout << argv[i];
-		}
-	}
+	bool resetTerminal = ! resetTermSwitch.getValue();
+	bool doEndl = ! dontEndlSwitch.getValue();
+
+	string color = colorArg.getValue();
+	string background = backgroundArg.getValue();
+	string effect = effectArg.getValue();
+	string resetEff = resetEffectArg.getValue();
+	
+	string text = textArg.getValue();
+
+	setColor(color);
+	setBackground(background);
+	setEffect(effect);
+	resetEffect(resetEff);
+	
+	cout << text;
+	
 	if(resetTerminal)
 		reset();
 	if(doEndl)
